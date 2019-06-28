e7f4df8b31220ba688a96bed5a2ac0dfc60ff9e3
Live session user <luk036@gmail.com>
2019-06-28 14:29:03 +0800

    try codecov

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -3,7 +3,7 @@ set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=ellcpp")
+set(CTEST_DROP_LOCATION "/submit.php?project=ckpttncpp")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
 # In case where you don't want to submit your reports to CDash,
